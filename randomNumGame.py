aa64e8dfa21356d47d776706439fec4911f1e8eb
0xskar <106286585+0xskar@users.noreply.github.com>
2023-01-03 17:30:06 -0800

    randomnumgame

--- randomNumGame.py
+++ randomNumGame.py
@@ -6,7 +6,21 @@ player1 = input("Name of Player 1: ")
 player2 = input("Name of Player 2: ")
 player3 = input("Name of Player 3: ")
 
-print(player1, "rolls a", random.randint(0, 100))
-print(player2, "rolls a", random.randint(0, 100))
-print(player3, "rolls a", random.randint(0, 100))
+player1roll = random.randint(0, 100)
+player2roll = random.randint(0, 100)
+player3roll = random.randint(0, 100)
+
+print(player1, "rolls a", player1roll)
+print(player2, "rolls a", player2roll)
+print(player3, "rolls a", player3roll)
+
+if player1roll > player2roll > player3roll :
+    print("player1 wins with a", player1roll)
+
+if player2roll > player3roll > player1roll :
+    print("player2 wins with a", player2roll)
+
+if player3roll > player2roll > player1roll :
+    print("player3 wins with a", player3roll)
+
 
