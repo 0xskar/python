7f2e3d027302bde00d87dd8075e8044e21ccfa75
0xskar <106286585+0xskar@users.noreply.github.com>
2023-01-03 22:08:12 -0800

    lists and data storage

---
+++ projectGrader.py
@@ -0,0 +1,20 @@
+
+
+gradePercent = int(input("Input the grade percentage:"))
+
+if gradePercent > 90 :
+    print("They got a AA")
+elif gradePercent > 85 :
+    print("They got a BA")
+elif gradePercent > 80 :
+    print("They got a BB")
+elif gradePercent > 75 :
+    print("They got a CB")
+elif gradePercent > 70 :
+    print("They got a CC")
+elif gradePercent > 65 :
+    print("They got a DC")
+elif gradePercent > 60 :
+    print("They got a DD")
+else :
+    print("They failed")
