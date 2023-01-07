5f03fa3fe0f43c9c089133d315a54dabd9836e08
0xskar <106286585+0xskar@users.noreply.github.com>
2023-01-07 02:37:09 -0800

    show collection

--- Projects/ShowCollection.py
+++ Projects/ShowCollection.py
@@ -8,7 +8,27 @@ connection = mysql.connector.connect(user='0xskar',
                                      password='passw0rd',
                                      host='192.168.0.34',
                                      database='ShowCollection')
+
+
+
+# check options
+def MAIN_MENU_SELECT(): 
+    input_pass = 0
+    while(input_pass == 0):
+        try:
+            menu_select = int(input("Please, select a option: "))
+            if menu_select <= int(7):
+                break
+            else:
+                print("Not a valid menu option.")
+        except ValueError:
+            print("Not a valid menu option.")
+        
+
+
+
 if connection.is_connected():
+
     print("             SHOW COLLECTION by 0xskar             ")
     print("***************************************************")
     print("                     MAIN MENU                     ")
@@ -21,16 +41,18 @@ if connection.is_connected():
     print("6) Delete a Movie        ")
     print("7) Delete a TV Show      ")
     print("***************************************************")
-    menu_options = [i for i in range(1, 7)]
-    menu_select = input("Please, select a option: ")
-    while(int(menu_select) not in menu_options):
-        print("Not a valid menu option.")
-        menu_select = input("Please, select a option: ")
-            
 
+    # list of integers as string
+    menu_options = [i for i in range(1, 8)]
+    # empty list to store integers
+    menu_choices = []
+    # convert options string to integers
+    for x in menu_options:
+        menu_choices.append(int(x))
 
+    print(menu_choices)
 
-
+    MAIN_MENU_SELECT()
 
     connection.close()
     print("Disconnected.")
