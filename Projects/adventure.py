7e3e9166fc2a2d842e67409f46a0d59e7fd832db
0xskar <106286585+0xskar@users.noreply.github.com>
2023-01-18 02:56:45 -0800

    working. going to add wager next

--- Projects/adventure.py
+++ Projects/adventure.py
@@ -6,16 +6,18 @@ import random
 import time
 import curses
 from curses import wrapper
+from curses.textpad import Textbox, rectangle
 
 
-# Curses Module
-# 
-#           clear screen:       stdscr.clear()
-#           refresh screen:     stdscr.refresh()
-#           end program on key: stdscr.getch()
-#           new window:         curses.newwin(5, 25, 0, 0)
+# Curses Module Commands Reference
 #
-# Curses Colors
+#   curses.newwin(a, b, c, d)   - creates new window                        a = window cell height
+#   curses.clear()              - clears screen                             b = window cell width
+#   curses.refresh()            - refreshes screen                          c = cells from the top
+#   curses.getch()              - waits for and gets a user input           d = cells from the right
+#   rectangle(curses, 0, 0, 3, 49)  - draws a rectangle
+#
+#   rectangle(window_dice_game, 0, 55, 3, 37)
 
 
 # Character Stats  These range from about 3 to 20.
@@ -87,6 +89,7 @@ class Player1:
         self.name = name
         self.roll = 0
         self.money = 0
+        self.score = 0
 
 
 class Player2:
@@ -94,83 +97,96 @@ class Player2:
         self.name = name
         self.roll = 0
         self.money = 0
+        self.score = 0
+
+
+# Curses Windows
+def window_title(RED_AND_BLACK):
+    window_title_location = curses.newwin(5, 50, 0, 0)
+    window_title_location.refresh()
+    rectangle(window_title_location, 0, 0, 3, 49)
+    window_title_location.addstr(1, 17, f'+ Star of Dawn +', RED_AND_BLACK)
+    window_title_location.addstr(2, 12, f' Chapter 1: Ashen Queen', RED_AND_BLACK)
+    window_title_location.refresh()
 
 
 def dice_game(player1_name, player1_money, player2_name, player2_money, rounds):
     player1 = Player1(player1_name)
     player2 = Player2(player2_name)
+    player1.money = main_character.money
 
-    window_dice_game_title = curses.newwin(0, 0, 0, 55)
-    window_dice_game_body = curses.newwin(0, 32, 4, 55)
+    if player2.name == companion_one.name:
+        player2.money = companion_one.money
 
+    # windows and colors
+    window_dice_game_title = curses.newwin(0, 37, 0, 55)
+    window_dice_game_body = curses.newwin(0, 37, 4, 55)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    RED_AND_BLACK = curses.color_pair(1)
     BLUE_AND_BLACK = curses.color_pair(2)
-    window_dice_game_title.clear()
-    window_dice_game_title.addstr(0, 0, f'--------------------------------')
-    window_dice_game_title.addstr(1, 0, f'+              Dice            +', BLUE_AND_BLACK)
-    window_dice_game_title.addstr(2, 0, f'--------------------------------')
-    window_dice_game_title.refresh()
+    GREEN_AND_BLACK = curses.color_pair(3)
 
+    # trying to get rectangle to draw
+    window_dice_game_title.clear()
+    rectangle(window_dice_game_title, 0, 0, 3, 36)
+    window_dice_game_title.addstr(1, 14, f'+ Dice +', BLUE_AND_BLACK)
+    window_dice_game_title.addstr(2, 11, f'Game of Chance', BLUE_AND_BLACK)
+    window_dice_game_title.refresh()
     window_dice_game_body.clear()
-    window_dice_game_body.addstr(0, 0, f'{player1.name}, {player1.money}. {player2.name}, {player2.money}.')
-    window_dice_game_body.addstr(2, 0, f'How much do you wager?')
-    window_dice_game_body.clear
+    window_dice_game_body.addstr(0, 0, f'How much do you wager?')
+    window_dice_game_body.addstr(2, 0, f'You have {player1.money}, and {player2.name} has {player2.money}.')
+
+    # wager = window_dice_game_body.getkey()
+    # window_dice_game_body.addstr(4, 0, f'Enter wager: {wager}')
     window_dice_game_body.refresh()
     window_dice_game_body.getch()
 
     dice_round = 0
 
     for i in range(rounds):
-        dice_round = dice_round + 1
+        dice_tie = 0
+        dice_round += 1
         window_dice_game_body.clear()
         window_dice_game_body.addstr(0, 0, f'Round {dice_round}')
         player1.roll = random.randint(1, 10)
-        window_dice_game_body.addstr(1, 0, f'{player1.name} rolls a {player1.roll}.')
+        window_dice_game_body.addstr(2, 0, f'{player1.name} rolls a {player1.roll}.')
         player2.roll = random.randint(1, 10)
-        window_dice_game_body.addstr(2, 0, f'{player2.name} rolls a {player2.roll}')
+        window_dice_game_body.addstr(3, 0, f'{player2.name} rolls a {player2.roll}.')
         if player1.roll > player2.roll:
-            window_dice_game_body.addstr(3, 0, f'{player1.name} wins with {player1.roll}')
+            window_dice_game_body.addstr(0, 30, f'WIN!!!', GREEN_AND_BLACK)
+            window_dice_game_body.addstr(5, 0, f'{player1.name} wins with {player1.roll}')
+            window_dice_game_body.addstr(7, 0, f'{player1.name}: {player1.score}')
+            window_dice_game_body.addstr(8, 0, f'{player2.name}: {player2.score}')
+            player1.score += 1
             window_dice_game_body.refresh()
             window_dice_game_body.getch()
         elif player1.roll < player2.roll:
-            window_dice_game_body.addstr(4, 0, f'{player2.name} wins with {player2.roll}')
-        else:
-            window_dice_game_body.addstr(0, 0, f'It\'s a tie')
-            dice_tie = 1
-            while dice_tie == 1:
-                try:
-                    window_dice_game_body.addstr(0, 0, f'Round {i + 1}')
-                    player1.roll = random.randint(1, 10)
-                    window_dice_game_body.addstr(1, 0, f'{player1.name} rolls a {player1.roll}.')
-                    player2.roll = random.randint(1, 10)
-                    window_dice_game_body.addstr(2, 0, f'{player2.name} rolls a {player2.roll}')
-                    if player1.roll > player2.roll:
-                        window_dice_game_body.addstr(3, 0, f'{player1.name} wins with {player1.roll}')
-                        dice_tie = 0
-                        time.sleep(1)
-                    if player1.roll < player2.roll:
-                        window_dice_game_body.addstr(3, 0, f'{player2.name} wins with {player2.roll}')
-                        dice_tie = 0
-                        time.sleep(1)
-                    elif player1.roll == player2.roll:
-                        window_dice_game_body.addstr(0, 0, f'It\'s a tie')
-                except dice_tie == 0:
-                    break
+            window_dice_game_body.addstr(0, 30, f'LOSS!!!', RED_AND_BLACK)
+            window_dice_game_body.addstr(5, 0, f'{player2.name} wins with {player2.roll}')
+            window_dice_game_body.addstr(7, 0, f'{player1.name}: {player1.score}')
+            window_dice_game_body.addstr(8, 0, f'{player2.name}: {player2.score}')
+            player2.score += 1
+            window_dice_game_body.refresh()
+            window_dice_game_body.getch()
+        elif player1.roll == player2.roll:
+            window_dice_game_body.addstr(0, 30, f'TIE!!!', BLUE_AND_BLACK)
+            window_dice_game_body.addstr(5, 0, f'{player1.name} and {player2.name} tie with a {player1.roll}.')
+            window_dice_game_body.addstr(7, 0, f'{player1.name}: {player1.score}')
+            window_dice_game_body.addstr(8, 0, f'{player2.name}: {player2.score}')
+            window_dice_game_body.refresh()
+            window_dice_game_body.getch()
+
+    if player1.score > player2.score:
+        print('Player 1 wins the game.')
+    elif player1.score < player2.score:
+        print('Player 1 loses the game.')
 
 
 # Sleep function
 def sleep(sec):
     time.sleep(sec)
-
-
-# Curses Windows
-def window_title(RED_AND_BLACK):
-    window_title_location = curses.newwin(5, 50, 0, 0)
-    window_title_location.clear()
-    window_title_location.addstr(0, 0, f'--------------------------------------------------')
-    window_title_location.addstr(1, 0, f'+           Star of Dawn - Introduction          +', RED_AND_BLACK)
-    window_title_location.addstr(2, 0, f'--------------------------------------------------')
-    window_title_location.refresh()
 
 
 # Game Scenes
@@ -185,7 +201,7 @@ def scene_boat():
                                    f'as you need to prepare for your contract.'
                                    f'Put on your clothes, your sword and sheathe and head on into the galley to see if {companion_one.name}, '
                                    f'your friend, is up as well.')
-    window_boat_scene.addstr(11, 0, f'       --== Press Any Key to Continue ==--')
+    window_boat_scene.addstr(11, 10, f'--== Press Any Key ==--')
 
     window_boat_scene.refresh()
     window_boat_scene.getch()
@@ -194,12 +210,12 @@ def scene_boat():
     window_boat_scene.clear()
     window_boat_scene.addstr(0, 0, f'Meeting {companion_one.name} in the Galley. The Barkeep is there as well.'
                                    f'They challenge you to round of dice, you love dice so accept it with no delay.')
-    window_boat_scene.addstr(11, 0, f'       --== Press Any Key to Continue ==--')
+    window_boat_scene.addstr(11, 10, f'--== Press Any Key ==--')
     window_boat_scene.refresh()
     window_boat_scene.getch()
 
-    # dice game
-    rounds = 3
+    # ask how much to wager and how many rounds (divisible)
+    rounds = 100
     dice_game(main_character.name, main_character.money, companion_one.name, companion_one.money, rounds)
 
     # calculate if lost a lot of the wager to insult, and if not to congratulate
@@ -215,8 +231,16 @@ def scene_boat():
 
 # main function. curses and colors and other variables
 def main(stdscr):
+    # colors
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
     RED_AND_BLACK = curses.color_pair(1)
+    BLUE_AND_BLACK = curses.color_pair(2)
+    GREEN_AND_BLACK = curses.color_pair(3)
+
+    # Screen Boxes
+
     window_title(RED_AND_BLACK)
     scene_boat()
 
